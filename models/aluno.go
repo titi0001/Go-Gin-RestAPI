f7ae0cc40ed1badc57abe4b302dc05cafce733c6
Thiago <titiura@gmail.com>
2024-04-09 10:24:17 -0300

    refactor: add gorm.model to student struct

--- models/aluno.go
+++ models/aluno.go
@@ -1,8 +1,12 @@
 package models
 
+import "gorm.io/gorm"
+
 type Aluno struct {
+	gorm.Model
 	Nome string `json:"nome"`
 	CPF  string `json:"CPF"`
 	RG   string `json:"RG"`
 }
 
+var Alunos []Aluno
