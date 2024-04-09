9445b1e34dd90aeec0853a73693c4652cbd66fa8
Thiago <titiura@gmail.com>
2024-04-09 11:51:29 -0300

    feat: add list get all

--- controllers/controller.go
+++ controllers/controller.go
@@ -1,17 +1,33 @@
 package controller
 
 import (
+	"net/http"
+
 	"github.com/gin-gonic/gin"
+	"github.com/titi0001/Go-Gin-RestAPI/database"
 	"github.com/titi0001/Go-Gin-RestAPI/models"
 )
-
-func ExibeAlunos(c *gin.Context) {
-	c.JSON(200, models.Alunos)
-}
 
 func Saudacao(c *gin.Context) {
 	nome := c.Params.ByName("nome")
 	c.JSON(200, gin.H{
 		"API diz:": "E ai " + nome + ", tudo beleze?",
 	})
+}
+
+func ExibeAlunos(c *gin.Context) {
+	var alunos []models.Aluno
+	database.DB.Find(&alunos)
+	c.JSON(200, alunos)
+}
+
+func CriarAluno(c *gin.Context) {
+	var aluno models.Aluno
+	if err := c.ShouldBindJSON(&aluno); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": err.Error()})
+		return
+	}
+	database.DB.Create(&aluno)
+	c.JSON(http.StatusOK, aluno)
 }
