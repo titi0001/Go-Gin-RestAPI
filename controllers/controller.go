11724da01fa6036053f1fc70465aa99451166954
Thiago <titiura@gmail.com>
2024-04-09 12:04:22 -0300

    feat: status code not found by id

--- controllers/controller.go
+++ controllers/controller.go
@@ -36,5 +36,11 @@ func BuscaAlunoPorId(c *gin.Context){
 	var aluno models.Aluno
 	id := c.Params.ByName("id")
 	database.DB.First(&aluno, id)
+	if aluno.ID == 0 {
+		c.JSON(http.StatusNotFound, gin.H{
+			"Not found": "Aluno não encontrado",
+		})
+		return
+	}
 	c.JSON(http.StatusOK, aluno)
 }
