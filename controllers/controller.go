a72e4cb58cd8d7d17136e192fb2e1836735dfaa5
Thiago Pereira <titiura@gmail.com>
2024-04-22 11:18:12 -0300

    fix: error write string

--- controllers/controller.go
+++ controllers/controller.go
@@ -11,7 +11,7 @@ import (
 func Saudacao(c *gin.Context) {
 	nome := c.Params.ByName("nome")
 	c.JSON(200, gin.H{
-		"API diz:": "E ai " + nome + ", tudo beleze?",
+		"API diz:": "E ai " + nome + ", tudo beleza?",
 	})
 }
 
